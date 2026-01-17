46e7056bce51e07246247bff9a5c2a77bf74f4e2
DanteSGarrett <dante.s.garrett@gmail.com>
2026-01-17 09:33:29 -0700

    commented out some things in requirements and added sparkMaxUtils to swervemodule.py to try to make this code work, although it is not currently working

--- subsystem/drivetrain/swerve_module.py
+++ subsystem/drivetrain/swerve_module.py
@@ -4,7 +4,7 @@ from typing import Tuple
 # Internal imports
 from config import OperatorRobotConfig
 from constants import SwerveModuleMk4iConsts, SwerveModuleMk4iL2Consts
-from raptacon3200.utils import sparkMaxUtils
+# from raptacon3200.utils import sparkMaxUtils
 
 # Third-party imports
 import phoenix6
@@ -23,6 +23,70 @@ https://store.ctr-electronics.com/cancoder/
 Other defaults may be added in future
 """
 
+# sparkMaxUtils stand-in
+
+# Third-party imports
+import rev
+
+#############################
+# CONSTANTS ###################
+#############################
+
+
+class SparkMaxConstants:
+    faultRateMs: int = 50
+    motorPosRateMs: int = 50
+    appliedOutputRateMs: int = 50
+    motorTelmRateMs: int = 50
+    analogRateMs: int = 1833
+    altEncoderRateMs: int = 1050
+    dutyCycleEncRateMs: int = 2150
+    dutyCycleEncVelRateMs: int = 3150
+
+
+
+def configureSparkMaxCanRates(
+    self,
+    config: rev.SparkMaxConfig,
+    drive_motor_flag: bool,
+    faultRateMs: int = SparkMaxConstants.faultRateMs,
+    motorPosRateMs: int = SparkMaxConstants.motorPosRateMs,
+    appliedOutputRateMs: int = SparkMaxConstants.appliedOutputRateMs
+) -> None:
+    """
+    Configure the data transfer type and rate for swerve drive SparkMaxs. Some configurations
+    are universal while others vary based on the given inputs.
+    Args:
+        config: the configuration object for the SparkMax to update
+        drive_motor_flag: if True, the SparkMax controls a drive motor on the swerve drive.
+            If False, it controls a steer motor
+        faultRateMs: the rate, in milliseconds, at which fault signals are transmitted
+        motorPosRateMs: the rate, in milliseconds, at which the position of the motor is transmitted
+        appliedOutputRateMs: the rate, in milliseconds, at which the motor's applied output is transmitted
+    Returns:
+        None - passed configuration is updated in-place
+    """
+    (
+        config.signals
+        # Fixed settings
+        .absoluteEncoderPositionAlwaysOn(False)
+        .absoluteEncoderVelocityAlwaysOn(False)
+        .analogPositionAlwaysOn(False)
+        .analogVelocityAlwaysOn(False)
+        .analogVoltageAlwaysOn(False)
+        .externalOrAltEncoderPositionAlwaysOn(False)
+        .externalOrAltEncoderVelocityAlwaysOn(False)
+        .primaryEncoderPositionAlwaysOn(True)
+        .IAccumulationAlwaysOn(False)
+
+        # Input settings
+        .primaryEncoderVelocityAlwaysOn(drive_motor_flag)
+        .primaryEncoderPositionPeriodMs(motorPosRateMs)
+        .appliedOutputPeriodMs(appliedOutputRateMs)
+        .faultsPeriodMs(faultRateMs)
+    )
+
+# end sparkMaxUtils
 
 class SwerveModuleMk4iSparkMaxNeoCanCoder:
     """
@@ -140,7 +204,7 @@ class SwerveModuleMk4iSparkMaxNeoCanCoder:
         Returns:
             None - internal steer configuration is updated in-place
         """
-        sparkMaxUtils.configureSparkMaxCanRates(self.steer_motor_config, drive_motor_flag=False)
+        self.configureSparkMaxCanRates(self.steer_motor_config, drive_motor_flag=False)
         (
             self.steer_motor_config
             .inverted(invert)
@@ -178,7 +242,7 @@ class SwerveModuleMk4iSparkMaxNeoCanCoder:
         Returns:
             None - internal drive configuration is updated in-place
         """
-        sparkMaxUtils.configureSparkMaxCanRates(self.drive_motor_config, drive_motor_flag=True)
+        self.configureSparkMaxCanRates(self.drive_motor_config, drive_motor_flag=True)
         (
             self.drive_motor_config
             .inverted(invert)
