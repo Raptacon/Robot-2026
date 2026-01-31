0bbdb3b4d711954b19d5b9c08521a6ead541c7a0
Nathan <tuffinmuffin@gmail.com>
2026-01-31 12:58:01 -0700

    test updates

--- examples/flywheel-sysid/flywheel.py
+++ examples/flywheel-sysid/flywheel.py
@@ -23,3 +23,24 @@ class FlywheelSysId(Subsystem):
         value = sysIdRoutine.dynamic(direction)
         print(f"Dynamic Command Value: {value}, Direction: {direction}")
         return sysIdRoutine.dynamic(direction)
+    
+    def sysIdLog(self, sys_id_routine: SysIdRoutineLog) -> None:
+        for name, motor in self.motors.items():
+            motor_log = sys_id_routine.motor(name)
+            #gather data
+            #angular veloicity and andgulat psotion
+            angular_velocity = motor.getEncoder().getVelocity() * (2 * 3.141592653589793 / 60)  # Convert RPM to rad/s
+            angular_position = motor.getEncoder().getPosition() * (2 * 3.141592653589793 / 60)  # Convert rotations to rad
+            current = motor.getOutputCurrent()
+            battery_voltage = motor.getBusVoltage()
+            motor_temp = motor.getMotorTemperature()
+            applied_voltage = motor.getAppliedOutput() * battery_voltage
+            
+            #not included velocity acceleration, angularAcceleration, angularPosition,
+            motor_log.angularVelocity(angular_velocity)
+            motor_log.position(angular_position)
+            motor_log.current(current)
+            motor_log.voltage(applied_voltage)
+            #extra data
+            motor_log.value("temperature", motor_temp, "C")
+            motor_log.value("busVoltage", battery_voltage, "V")
--- examples/flywheel-sysid/robot.py
+++ examples/flywheel-sysid/robot.py
@@ -16,10 +16,8 @@ motorIdNameMap = {
     kIntakeFlywheelMotorID: "IntakeFlyWheel",
 }
 
-def GetSparkSignalsConfig(periodMs: int) -> rev.SignalsConfig:
-    signalConfig = rev.SignalsConfig()
-    signalConfig.analogVoltageAlwaysOn(True)
-    signalConfig.analogPositionPeriodMs(periodMs)
+def GetSparkSignalsVelocityControlConfig(signalConfig: rev.SignalsConfig, periodMs: int) -> rev.SignalsConfig:
+    #signalConfig = rev.SignalsConfig()
     signalConfig.busVoltageAlwaysOn(True)
     signalConfig.busVoltagePeriodMs(periodMs)
     signalConfig.appliedOutputAlwaysOn(True)
@@ -30,44 +28,69 @@ def GetSparkSignalsConfig(periodMs: int) -> rev.SignalsConfig:
     signalConfig.outputCurrentPeriodMs(periodMs)
     signalConfig.primaryEncoderVelocityAlwaysOn(True)
     signalConfig.primaryEncoderVelocityPeriodMs(periodMs)
-    signalConfig.setpointAlwaysOn(True)
-    signalConfig.setpointPeriodMs(periodMs)
+    
+    #signalConfig.setpointAlwaysOn(True)
+    #signalConfig.setpointPeriodMs(periodMs)
     return signalConfig
 
-def GetSparkConfig(inverted: bool = False, 
-                   periodMs: int = 20, 
-                   idleMode: rev.SparkBaseConfig.IdleMode = rev.SparkBaseConfig.IdleMode.kCoast ) -> rev.SparkBaseConfig:
-    config = rev.SparkBaseConfig()
+def GetSparkConfig(
+    config: rev.SparkBaseConfig,
+    inverted: bool = False, 
+    periodMs: int = 10, 
+    idleMode: rev.SparkBaseConfig.IdleMode = rev.SparkBaseConfig.IdleMode.kCoast ) -> rev.SparkBaseConfig:
+    #config = rev.SparkBaseConfig()
     config.inverted(inverted)
     config.setIdleMode(idleMode)
     config.voltageCompensation(12.0)
-    config.apply(GetSparkSignalsConfig(periodMs))
+    #config.apply()
+    GetSparkSignalsVelocityControlConfig(config.signals, periodMs)
     return config
+
+def GetFlywheelPidConfig(p : float = 0.0, i : float = 0.0, d : float = 0.0, outputRange : list[float] = [-1.0, 1.0], kV : float = 0.0) -> rev.ClosedLoopConfig:
+    pidConfig = rev.ClosedLoopConfig()
+    if len(outputRange) != 2:
+        raise ValueError("outputRange must be a list of two floats: [minOutput, maxOutput]")
+    pidConfig.P(p)
+    pidConfig.I(i)
+    pidConfig.D(d)
+    pidConfig.feedForward.kV(kV)
+    pidConfig.minOutput(outputRange[0])
+    pidConfig.maxOutput(outputRange[1])
+    return pidConfig
+
 
 class MyRobot(TimedCommandRobot):
     def robotInit(self):
         flywheelMotors = {}
-        motor = rev.SparkFlex(1, rev.SparkLowLevel.MotorType.kBrushless)
-        motor.configure(GetSparkConfig(), rev.ResetMode.kResetSafeParameters, rev.PersistMode.kNoPersistParameters)
+        motor = rev.SparkFlex(10, rev.SparkLowLevel.MotorType.kBrushless)
+        print(motor.configAccessor.signals.getPrimaryEncoderVelocityAlwaysOn())
+        config = GetSparkConfig(rev.SparkBaseConfig())
+        errror = motor.configure(config, rev.ResetMode.kResetSafeParameters, rev.PersistMode.kNoPersistParameters)
+        print(errror)
+        print(motor.configAccessor.signals.getPrimaryEncoderVelocityAlwaysOn())
+        GetFlywheelPidConfig()
         flywheelMotors["upperFlyWheel"] = motor
-        motor = rev.SparkFlex(2, rev.SparkLowLevel.MotorType.kBrushless)
-        motor.configure(GetSparkConfig(), rev.ResetMode.kResetSafeParameters, rev.PersistMode.kNoPersistParameters)
+        motor = rev.SparkMax(6, rev.SparkLowLevel.MotorType.kBrushless)
+        motor.configure(config, rev.ResetMode.kResetSafeParameters, rev.PersistMode.kNoPersistParameters)
         flywheelMotors["lowerFlyWheel"] = motor
 
-        motor = rev.SparkMax(3, rev.SparkLowLevel.MotorType.kBrushless)
-        motor.configure(GetSparkConfig(), rev.ResetMode.kResetSafeParameters, rev.PersistMode.kNoPersistParameters)
+        motor = rev.SparkFlex(14, rev.SparkLowLevel.MotorType.kBrushless)
+        motor.configure(config, rev.ResetMode.kResetSafeParameters, rev.PersistMode.kNoPersistParameters)
         flywheelMotors["intakeFlyWheel"] = motor
 
         self.flywheels = flywheel.FlywheelSysId(flywheelMotors)
 
         #setup logging
         wpilib.DataLogManager.start()
-        urcl.start(motorIdNameMap)
+        urcl.start()
+        #urcl.start(motorIdNameMap, wpilib.DataLogManager.getLog())
 
-        sysIdConfig = SysIdRoutine.Config(1, 7, 10.0, None)
-        sysIdMechanism = SysIdRoutine.Mechanism(self.flywheels.setMotorVoltage, self.sysIdNullLog, self.flywheels, "Flywheels")
+        sysIdConfig = SysIdRoutine.Config(2, 8, 10.0, None)
+        sysIdMechanism = SysIdRoutine.Mechanism(self.flywheels.setMotorVoltage, self.flywheels.sysIdLog, self.flywheels, "Flywheels")
         self.sysId = SysIdRoutine(sysIdConfig, sysIdMechanism)
 
+    
+    def teleopInit(self) -> None:
         self.controller = CommandXboxController(0)
 
         self.controller.a().whileTrue(
@@ -82,6 +105,9 @@ class MyRobot(TimedCommandRobot):
         self.controller.y().whileTrue(
             self.flywheels.sysIdDynamicCommand(SysIdRoutine.Direction.kReverse, self.sysId)
         )
-
-    def sysIdNullLog(self, sys_id_routine: SysIdRoutineLog) -> None:
-        pass
+        #print(dir(self.flywheels.motors["upperFlyWheel"])PeriodicFrame)
+        
+    
+    def teleopPeriodic(self):
+        super().teleopPeriodic()
+        
