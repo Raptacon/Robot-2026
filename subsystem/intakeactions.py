aa36dc74ea509b3a386e6acd56b6c740c3383826
nazeermazeer <234304154+nazeermazeer@users.noreply.github.com>
2026-02-14 10:33:46 -0700

    network tables + limit updates
    - code stops motor instead of code when limits are exceeded
    - adds 4 new variables to network tables

--- subsystem/intakeactions.py
+++ subsystem/intakeactions.py
@@ -142,11 +142,12 @@ class IntakeSubsystem(commands2.SubsystemBase):
 
     def motorChecks(self):
         if self.intakeMotorEncoder.getPosition() >= self.intakeDeployed + 15 and self.intakeCondition == 1:
-            print("INTAKE ERR121: Intake Motor appears to be deploying outside of limits! Stopping Code.")
-            os._exit(121)
+            print("INTAKE ERR121: Intake Motor appears to be deploying outside of limits! Motor has been stopped.")
+            self.intakeMotor.stopMotor()
         if self.intakeMotorEncoder.getPosition() <= self.intakeStowed - 15 and self.intakeCondition == -1:
-            print("INTAKE ERR122: Intake Motor appears to be stowing outside of limits! Stopping Code.")
-            os._exit(122)
+            print("INTAKE ERR122: Intake Motor appears to be stowing outside of limits! Motor has been stopped.")
+            self.intakeMotor.stopMotor()
+        
         if self.intakeMotorEncoder.getPosition() >= self.intakeDeployed and self.intakeCondition == 1:
             self.intakeCondition = 0
         if self.intakeMotorEncoder.getPosition() <= self.intakeStowed and self.intakeCondition == -1:
@@ -190,6 +191,10 @@ class IntakeSubsystem(commands2.SubsystemBase):
         wpilib.SmartDashboard.putNumber("Intake Condition", self.intakeCondition)
         wpilib.SmartDashboard.putBoolean("Break Beam Sensor", self.breakBeam.get())
         wpilib.SmartDashboard.putNumber("Roller Sensor", self.rollerSensor)
+        wpilib.SmartDashboard.putNumber("Intake Difference", self.intakeDifference)
+        wpilib.SmartDashboard.putNumber("Remaining Rotations", self.remainingRotations)
+        wpilib.SmartDashboard.putNumber("Intake Slowdown Position", self.intakeSlowdownPosition)
+        wpilib.SmartDashboard.putNumber("Intake Voltage", self.intakeMotorEncoder.getOutputCurrent())
         
         self.motorChecks()
         self.automaticRollerActivation()
