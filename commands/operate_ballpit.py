75f44677f1f2a1c9b7ec0b73298f0621699b8844
DanteSGarrett <dante.s.garrett@gmail.com>
2026-02-21 11:34:52 -0700

    added docstrings for OperateBallPit functions

--- commands/operate_ballpit.py
+++ commands/operate_ballpit.py
@@ -3,6 +3,17 @@ from subsystem.ballpit import BallPitHopper
 
 class OperateBallPit(commands2.Command):
     def __init__(self, hopper: BallPitHopper, isStuck: bool) -> None:
+        """
+        Adds requirements & initializes class OperateBallPit
+
+        Args: 
+            hopper: the Hopper Subsystem that this command will operate
+            isStuck: True if game pieces are stuck in the hopper
+
+        Returns: 
+            None - class initialization executed upon construction
+        
+        """
         super().__init__()
         self.hopper = hopper
         self.isStuck = isStuck
@@ -10,6 +21,12 @@ class OperateBallPit(commands2.Command):
         self.addRequirements(self.hopper)
 
     def execute(self):
+        """
+        Runs the Hex Shaft in the Hopper based on if there is a game piece stuck or not.
+        
+        Returns:
+            None - this is just executing operations
+        """
         if(self.isStuck):
             self.hopper.runHexShaft(-0.75)
         else:
