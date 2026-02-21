1269bb3b65c6e06f374bae0a2b6e34ef204d34ca
DanteSGarrett <dante.s.garrett@gmail.com>
2026-02-21 11:50:50 -0700

    added docstrings to ballpit

--- subsystem/ballpit.py
+++ subsystem/ballpit.py
@@ -3,7 +3,22 @@ import rev
 
 class BallPitHopper(commands2.SubsystemBase):
     def __init__(self) -> None:
-        self.intakeMotor = rev.SparkMax(21, rev.SparkLowLevel.MotorType.kBrushless)
+        """
+        Creates the hopper motor
+        
+        Returns:
+            None - class initialization executed upon construction
+        """
+        self.hopperMotor = rev.SparkMax(21, rev.SparkLowLevel.MotorType.kBrushless)
 
     def runHexShaft(self, percent : float):
-        self.intakeMotor.set(percent)
+        """
+        Sets the motor percentage for use elsewhere
+        
+        Args: 
+            percent: speed of the hopper motor from -1.0 - 1.0
+
+        Returns: 
+            None - function to be used elsewhere
+        """
+        self.hopperMotor.set(percent)
