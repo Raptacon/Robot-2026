471bf1a1eeadc6d369a1772bfd42cc57836df40b
nazeermazeer <234304154+nazeermazeer@users.noreply.github.com>
2026-02-23 20:20:15 -0700

    Revert robot intake to swerve
    Co-authored-by: Chris Hirsch <chris@base2technology.com>

--- robot.py
+++ robot.py
@@ -32,7 +32,7 @@ class MyRobot(commands2.TimedCommandRobot):
         if not hasattr(self, "container"):
             # to work around sim creating motors during tests, assign to class and if already created keep using created robot class for tests.
             # during robot running, this is only every called once
-            MyRobot.container = RobotIntake(lambda: self.isDisabled)
+            MyRobot.container = RobotSwerve(lambda: self.isDisabled)
 
     def robotInit(self) -> None:
         """
