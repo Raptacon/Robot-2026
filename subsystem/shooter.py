80c2d14cf60b4f6092036394667df13ee1c2d37f
Ken Wong <kwong4271@gmail.com>
2026-03-09 19:01:53 -0600

    Shooter Subsystem + Usage example (#52)
    * Swerve: include a feedforward in swerve drive motoro contorl
    
    * Sewrve: update configuration after odometry testing
    
    * Swerve: update COF on wheels after using dummy config for testing
    
    * Swerve: tune PID after implementing feedforward
    
    * SmartDashboard variable which controls drivetrain speed
    
    * Swerve: reposiition the speed dampener
    
    * Clean out temp speed dampener code
    
    * Initial 2026 code
    
    * cleaned up robotswerve.py file and removed other robot configs
    
    * cleaned out telemetry.py file
    
    * cleaned up vision.py file
    
    * cleaned the robotswerve.py file more
    
    * cleaned up imports in robotswerve.py file -- does not currently crash
    
    * cleaned up constants.py imports
    
    * commented out some things in requirements and added sparkMaxUtils to swervemodule.py to try to make this code work, although it is not currently working
    
    * Requirements: update dependencies for 2026 further
    
    * Vision: update API for 2026 version
    
    * sparkMaxConfg is now working, but there are still some errors with rev
    
    * fixed the rev errors, code now works in sim
    
    * Shooter code
    
    * resolved dependency issues
    
    * Inital Shooter Code
    
    * Basic shooter code
    
    * Added use of PIDF and PIDF configs to code
    
    * Added more functions and docstrings + added configs
    
    * Added calculations for distance and RPM, created lookup table for RPM, created methods to change the offset of RPM determined by calculations or lookup table
    
    * Shooter: clean up unused imports
    
    * clean up shooter code and leave notes on next steps
    
    * Shooter: clean up organization of shooter code
    
    * Shooter: update hwo all motor references are set in test mode
    
    * Shooter: rename shooter container
    
    * Shooter: update naming to match terminology used by team
    
    * Shooter: change motor references to use a StrEnum
    
    * Shooter: update the test code to set RPM instead of PID reference
    
    * Shooter: update CAN IDs
    
    * Shooter: update how Spark motors are configured to use a method
    
    * Shooter: update motor naming and test setup in shooter container
    
    * Shooter: set motor voltages to zero upon disable
    
    * Shooter: clean up prints and usage of shooter functionality
    
    * Shooter: set the follower on the flywheel as a follower
    
    * Shooter: update comment
    
    * Shooter: linting
    
    * Shooter: linting
    
    * Shooter: linting
    
    * Shooter: finish setup of leader follower on flywheel
    
    * Small code cleanup and fixes
    
    * Fixed issues pointed out by Nathan's review
    
    * commented out setDefaultCommand
    
    * typo in code
    
    * bug fix
    
    * Implemented interpolation of lookuptable
    
    * Fix robot reference
    
    * Shooter: clean unused import
    
    * linting
    
    * Moved shooter to its own folder in examples
    
    ---------
    
    Co-authored-by: Jordan Durham <jtd_29@yahoo.com>
    Co-authored-by: DanteSGarrett <dante.s.garrett@gmail.com>

---
+++ subsystem/shooter.py
@@ -0,0 +1,212 @@
+from config import ShooterConfig
+import rev
+import wpilib
+from commands2 import Subsystem
+from typing import Dict
+from enum import StrEnum
+import numpy as np
+
+class ShooterMotorNames(StrEnum):
+    """
+    Create consistent names for shooter motor references
+    """
+
+    FEED = "feed"
+    LEAD_FLYWHEEL = "lead"
+    FOLLOWER_FLYWHEEL = "follower"
+
+
+class Shooter(Subsystem):
+    def __init__(self):
+        super().__init__()
+        self.offsetAmount = 0
+        self.RPM = 0
+
+        # Create lookup table (distance, RPM)
+        self.lookupTable = [
+            (0.0, 1000),
+            (1.0, 1500),
+            (2.0, 2000),
+            (3.0, 3000),
+            (4.0, 3500),
+            (5.0, 4000),
+            ]
+        self.lookupTable.sort()
+        # Create an array of just distances
+        self.lookupShooterDistances = np.array([d for d, _ in self.lookupTable])
+        # Create an array of just RPMs
+        self.lookupShooterRpms = np.array([r for _, r in self.lookupTable])
+
+        # Instantiate motors
+        self.feedMotor = rev.SparkMax(30, rev.SparkLowLevel.MotorType.kBrushless)
+        self.leadFlywheelMotor = rev.SparkFlex(32, rev.SparkLowLevel.MotorType.kBrushless)
+        self.followerFlywheelMotor = rev.SparkFlex(33, rev.SparkLowLevel.MotorType.kBrushless)
+
+        # Set up configs for each motor
+        self.configureMotor(self.feedMotor, ShooterConfig.shooterFeedMotorPIDF, ShooterConfig.shooterInverted[0])
+        self.configureMotor(self.leadFlywheelMotor, ShooterConfig.shooterFlywheelMotorPIDF, ShooterConfig.shooterInverted[1])
+        self.configureMotor(self.followerFlywheelMotor, ShooterConfig.shooterFlywheelMotorPIDF, ShooterConfig.shooterInverted[2], self.leadFlywheelMotor)
+
+        self.motors: Dict[str, rev.SparkFlex | rev.SparkMax] = {
+            ShooterMotorNames.FEED: self.feedMotor,
+            ShooterMotorNames.LEAD_FLYWHEEL: self.leadFlywheelMotor,
+            ShooterMotorNames.FOLLOWER_FLYWHEEL: self.followerFlywheelMotor
+        }
+
+        # Get encoders from each motor to read data
+        self.feedEncoder = self.feedMotor.getEncoder()
+        self.leadFlywheelEncoder = self.leadFlywheelMotor.getEncoder()
+        self.followerFlywheelEncoder = self.followerFlywheelMotor.getEncoder()
+        self.encoders = {
+            ShooterMotorNames.FEED: self.feedEncoder,
+            ShooterMotorNames.LEAD_FLYWHEEL: self.leadFlywheelEncoder,
+            ShooterMotorNames.FOLLOWER_FLYWHEEL: self.followerFlywheelEncoder
+        }
+
+        # Create closed loop controllers to be able to set a reference/goal for pid
+        self.feedPID = self.feedMotor.getClosedLoopController()
+        self.leadFlywheelPID = self.leadFlywheelMotor.getClosedLoopController()
+        self.PIDs = {
+            ShooterMotorNames.FEED: self.feedPID,
+            ShooterMotorNames.LEAD_FLYWHEEL: self.leadFlywheelPID,
+            # Avoid key errors
+            ShooterMotorNames.FOLLOWER_FLYWHEEL: self.leadFlywheelPID,
+        }
+
+    def configureMotor(
+        self, motor: rev.SparkFlex | rev.SparkMax,
+        pidf: tuple,
+        invert: bool,
+        leader: rev.SparkFlex | rev.SparkMax = None
+    ):
+        """
+        Configure the PIDF constants and inversion for the given motor.
+        
+        Args:
+            motor: the motor on the shooter to configure
+            pidf: the PIDF constants to set on the given motor
+            invert: if True, invert the rotation direction of the given motor
+            leader: the motor to follow. If None, do not set this motor as a follower
+
+        Returns:
+            None
+        """
+        configs = rev.SparkBaseConfig()
+
+        if leader is not None:
+            configs.follow(leader=leader, invert=invert)
+        else:
+            configs.inverted(invert)
+            configs.closedLoop.pidf(*pidf, rev.ClosedLoopSlot.kSlot0)
+
+        motor.configure(configs, rev.ResetMode.kResetSafeParameters, rev.PersistMode.kPersistParameters)
+
+    def setMotorVoltage(self, motorName: str, voltage: float):
+        """
+        Sets the voltage of the motor
+
+        Args:
+            motorName: Name of the motor to set a voltage for (ie: 'feed')
+            voltage: Voltage to set the motor at
+
+        Returns:
+            None
+        """
+        self.motors[motorName].setVoltage(voltage)
+
+    def setMotorReference(self, motorName: str, rpm: float):
+        """
+        Give a custom setpoint for PID to achieve in terms of velocity
+
+        Args:
+            motorName: Name of the motor
+            rpm: The velocity setpoint for the motor in RPM
+
+        Returns:
+            None
+        """
+        self.PIDs[motorName].setReference(rpm, rev.SparkLowLevel.ControlType.kVelocity, rev.ClosedLoopSlot.kSlot0)
+
+    def setRPM(self, rpm: float):
+        """
+        Directly set the RPM using the given value
+
+        Args:
+            rpm: The velocity setpoint for the motor in RPM
+
+        Returns:
+            None
+        """
+        self.RPM = rpm
+
+    def getVelocity(self, motorName: str):
+        """
+        Get the current velocity of a motor in RPM
+
+        Args:
+            motorName: Name of the motor
+
+        Returns:
+            Velocity of the motor in RPM
+        """
+        return self.encoders[motorName].getVelocity()
+
+    def setRpmUsingLookup(self, distance: float):
+        """
+        Set the RPM needed to shoot the ball at a specified distance
+
+        Args:
+            distance: distance in meters from a target point
+
+        Returns:
+            None
+        """
+        # Get RPM from the distance given
+        self.RPM = float(np.interp(distance, self.lookupShooterDistances, self.lookupShooterRpms))
+
+    def modifyOffset(self, offsetDelta: float):
+        """
+        Modify the RPM offset
+
+        Args:
+            offsetDelta: change in offset that is applied
+
+        Returns:
+            None
+        """
+        self.offsetAmount = self.offsetAmount + offsetDelta
+
+    def resetOffset(self):
+        """
+        Reset the RPM offset
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        self.offsetAmount = 0
+
+    def getOffset(self):
+        """
+        Get the RPM offset
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        return self.offsetAmount
+
+    def periodic(self):
+        newRPM = self.RPM + self.offsetAmount
+        feedRPM = int(newRPM * ShooterConfig.shooterFeedPercentOfFlywheel)
+
+        self.setMotorReference(ShooterMotorNames.FEED, feedRPM)
+        self.setMotorReference(ShooterMotorNames.LEAD_FLYWHEEL, newRPM)
+
+        wpilib.SmartDashboard.putNumber("Shooter_RPM", newRPM)
+        wpilib.SmartDashboard.putNumber("Shooter_Feed_RPM", feedRPM)
+        wpilib.SmartDashboard.putNumber("Shooter_Offset", self.offsetAmount)
