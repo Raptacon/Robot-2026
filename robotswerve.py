cec31c64b334ff6f15d51bb7cec1c43fdfd873ba
unknown <kwong4271@gmail.com>
2026-03-21 11:28:33 -0600

    constantly calculate range for shooter and set rpm + removed unnecessary stuff

--- robotswerve.py
+++ robotswerve.py
@@ -14,8 +14,6 @@ import os
 from pathlib import Path
 from typing import Callable
 
-import wpimath
-
 # Internal imports
 from config import ShooterConfig
 from constants.swerve_constants import BallpitConstants
@@ -139,7 +137,6 @@ class RobotSwerve:
         pass
 
     def teleopInit(self):
-        commands2.CommandScheduler.getInstance().getDefaultButtonLoop().clear()
         self.updateAlliance()
         if self.auto_command:
             self.auto_command.cancel()
@@ -154,10 +151,9 @@ class RobotSwerve:
             )
         )
 
-        # TODO: Get odometry from drivetrain and calculate range
         self.shooter.setDefaultCommand(commands2.cmd.select(
             {
-                "autoRPM": commands2.cmd.run(lambda: self.shooter.setRpmUsingLookup(1), self.shooter),
+                "autoRPM": commands2.cmd.run(lambda: self.shooter.setRpmUsingLookup(self.shooter.calculateRangeFromOdometry(self.drivetrain.current_pose, self.alliance)), self.shooter),
                 "fixedRPM": commands2.cmd.run(lambda: self.shooter.setRPM(ShooterConfig.shooterFixedRPM), self.shooter)
             },
             self.shooter.getFlywheelMode
