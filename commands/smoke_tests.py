69c4c94f44a5754db5f95f28665b9ff800716fe3
nazeermazeer <234304154+nazeermazeer@users.noreply.github.com>
2026-03-22 10:20:21 -0600

    test messasges initial code

--- commands/smoke_tests.py
+++ commands/smoke_tests.py
@@ -6,6 +6,7 @@ from wpimath.geometry import Rotation2d
 from wpimath.kinematics import SwerveModuleState
 
 import commands2
+import wpilib
 
 class SmokeTests(commands2.SequentialCommandGroup):
     """
@@ -23,33 +24,41 @@ class SmokeTests(commands2.SequentialCommandGroup):
 
         self.drivetrain = drivetrain
         self.progress = False
+        self.testMessage = ""
         # self.velocity_vector_x = velocity_vector_x
         # self.velocity_vector_y = velocity_vector_y
         # self.angular_velocity = angular_velocity
 
         self.addRequirements(self.drivetrain)
         for swerve_module in self.drivetrain.swerve_modules:
-            self.addCommands(
+            if swerve_module == 0:
+                self.addCommands(commands2.cmd.runOnce(self.setMessage(1)))
                 commands2.WaitUntilCommand(lambda: self.progress).finallyDo(lambda _: self.advance(False)),
+            self.addCommands(
                 # Current swerve module's drive motor moves forward 0.2 meters per second until driver confirms 
                 commands2.InstantCommand(lambda swerve_module=swerve_module: swerve_module.set_state(SwerveModuleState(0.2, Rotation2d.fromDegrees(0)), apply_cosine_scaling=False),
                                         self.drivetrain),
+                commands2.cmd.runOnce(self.setMessage((swerve_module*6)+2, str(swerve_module.getName()), "Module starts driving")),
                 commands2.WaitUntilCommand(lambda: self.progress).finallyDo(lambda _: self.advance(False)),
                 # Current swerve module's drive motor stops
                 commands2.InstantCommand(lambda swerve_module=swerve_module: swerve_module.set_state(SwerveModuleState(0, Rotation2d.fromDegrees(0)), apply_cosine_scaling=False),
                                         self.drivetrain),
+                commands2.cmd.runOnce(self.setMessage((swerve_module*6)+3, str(swerve_module.getName()), "Module stops driving")),
                 commands2.WaitUntilCommand(lambda: self.progress).finallyDo(lambda _: self.advance(False)),
                 # Current swerve module's steer motor rotates to 0 degrees and driver confirms
                 commands2.InstantCommand(lambda swerve_module=swerve_module: swerve_module.set_state(SwerveModuleState(0, Rotation2d.fromDegrees(0)), apply_cosine_scaling=False),
                                         self.drivetrain),
+                commands2.cmd.runOnce(self.setMessage((swerve_module*6)+4, str(swerve_module.getName()), "Module rotates facing 0 degrees")),
                 commands2.WaitUntilCommand(lambda: self.progress).finallyDo(lambda _: self.advance(False)),
                 # Current swerve module's steer motor rotates to 45 degrees and driver confirms
                 commands2.InstantCommand(lambda swerve_module=swerve_module: swerve_module.set_state(SwerveModuleState(0, Rotation2d.fromDegrees(45)), apply_cosine_scaling=False),
                                         self.drivetrain),
+                commands2.cmd.runOnce(self.setMessage((swerve_module*6)+5, str(swerve_module.getName()), "Module rotates facing 45 degrees")),
                 commands2.WaitUntilCommand(lambda: self.progress).finallyDo(lambda _: self.advance(False)),
                 # Current swerve module's steer motor rotates to 90 degrees and driver confirms
                 commands2.InstantCommand(lambda swerve_module=swerve_module: swerve_module.set_state(SwerveModuleState(0, Rotation2d.fromDegrees(90)), apply_cosine_scaling=False),
                                         self.drivetrain),
+                commands2.cmd.runOnce(self.setMessage((swerve_module*6)+6, str(swerve_module.getName()), "Module rotates facing 90 degrees")),
                 commands2.WaitUntilCommand(lambda: self.progress).finallyDo(lambda _: self.advance(False)),
                 # Current swerve module's steer motor rotates back to 0 degrees when finished
                 commands2.InstantCommand(lambda swerve_module=swerve_module: swerve_module.set_state(SwerveModuleState(0, Rotation2d.fromDegrees(0)), apply_cosine_scaling=False),
@@ -61,3 +70,20 @@ class SmokeTests(commands2.SequentialCommandGroup):
 
     def advance(self, progress):
         self.progress = progress
+
+    def setMessage(self, testNumber, testComponent = None, testOutcome = None):
+        # self.testMessage = self.allMessages[messageNumber]
+        self.testNumber = testNumber
+        self.testComponent = testComponent
+        self.testOutcome = testOutcome
+        if self.testNumber == 1:
+            self.testMessage = """1/21:
+            \nBeginning of tests. Press start button when expected outcome is satisfied to move onto next test."""
+        else:
+            self.testMessage = """{self.testNumber}/21:
+            \nTesting {self.testComponent}...
+            \nEXPECTED OUTCOME: {self.testOutcome}
+            """
+
+    def updateMessage(self):
+        wpilib.SmartDashboard.putString("Test Message", self.testMessage)
