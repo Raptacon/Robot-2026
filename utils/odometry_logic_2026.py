d1dd1c762750cb43218f36923a2d34e9607b11f9
Jordan Durham <jtd_29@yahoo.com>
2026-03-22 15:23:01 -0600

    Utils: write a function to derive a shooter target from the current drivetrain odometry

---
+++ utils/odometry_logic_2026.py
@@ -0,0 +1,35 @@
+import wpilib
+from wpimath.geometry import Pose2d, Translation2d
+from constants.field_target_constants_2026 import FieldTargets2026
+from typing import Callable
+
+
+def determineShooterTargets2026(odometry: Callable[[],Pose2d], alliance: wpilib.DriverStation.Alliance):
+    """
+    Using the odometer and the robot's alliance, determine where on the field the shooter
+    should target.
+
+    Args:
+        odometry: polled current location and orientation of the robot on the field
+        alliance: red or blue
+
+    Returns:
+        target: the field location the shooter should target
+    """
+    odometryTranslation = odometry().translation()
+    target = Translation2d(8.270494, 4.034536)
+    if alliance == wpilib.DriverStation.Alliance.kRed:
+        if odometryTranslation.X() > FieldTargets2026.redHubTarget[0]: 
+            target = FieldTargets2026.redHubTarget
+        elif odometryTranslation.Y() < FieldTargets2026.redHubTarget[1]:
+            target = FieldTargets2026.bottomRightTarget
+        else:
+            target = FieldTargets2026.topRightTarget
+    else:
+        if odometryTranslation.X() < FieldTargets2026.blueHubTarget[0]:
+            target = FieldTargets2026.blueHubTarget
+        elif odometryTranslation.Y() < FieldTargets2026.blueHubTarget[1]:
+            target = FieldTargets2026.bottomLeftTarget
+        else:
+            target = FieldTargets2026.topLeftTarget
+    return target
